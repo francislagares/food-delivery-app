6bf635cb8822998b666355c6d58b3efeb627d2d5
Francis Lagares <francis.lagares@gmail.com>
2021-06-01 18:01:19 +0200

    feat(layout): create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from 'components/Layout/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className='app'>
-      <h1>Hello React App</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
