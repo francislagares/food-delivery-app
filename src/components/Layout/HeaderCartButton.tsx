8225207af29c8c59d765657e50a41e45f887b7a7
Francis Lagares <francis.lagares@gmail.com>
2021-06-03 18:49:26 +0200

    feat(layout): add cart-context to cart button

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from 'components/Cart/CartIcon';
+import CartContext from 'context/cart-context';
 
 interface Props {
   btnType: 'button' | 'reset' | 'submit';
@@ -9,13 +10,19 @@ interface Props {
 }
 
 const HeaderCartButton = ({ btnType, children, onClick }: Props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button type={btnType} className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>{children}</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
