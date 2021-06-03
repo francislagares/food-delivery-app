88b58c79b26aaa684f3d85c4d3fdf9ebc05eda5e
Francis Lagares <francis.lagares@gmail.com>
2021-06-03 18:54:02 +0200

    feat(types): define interfaces for reducer action types

--- src/types.d.ts
+++ src/types.d.ts
@@ -12,5 +12,28 @@ interface ICartItem {
   price: number;
 }
 
+type CartState = {
+  items: ICartItem[];
+  totalAmount: number;
+};
+
+enum ActionType {
+  IAddItem = 'ADD_ITEM',
+  IRemoveItem = 'REMOVE_ITEM',
+}
+
+interface IAddItem {
+  type: ActionType.IAddItem;
+  item: ICartItem;
+}
+
+interface IRemoveItem {
+  type: ActionType.IRemoveItem;
+  id: ICartItem['id'];
+}
+
+type CartAction = IAddItem | IRemoveItem;
+
+// Event Types
 type Submit = React.FormEvent<HTMLFormElement>;
 type ChangeInput = React.ChangeEvent<HTMLInputElement>;
