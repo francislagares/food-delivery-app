038e5e7d8f02b5d43b074aaf1e1704268caf4bfb
Francis Lagares <francis.lagares@gmail.com>
2021-06-04 17:04:53 +0200

    feat(meals): forward ref from Input to MealItemForm component

--- src/components/Meals/MealItem/MealItemForm.tsx
+++ src/components/Meals/MealItem/MealItemForm.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from 'components/UI/Input';
 
-const MealItemForm = () => {
+interface Props {
+  onAddToCart: (amount: number) => void;
+}
+
+const MealItemForm = ({ onAddToCart }: Props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: Submit) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current?.value;
+
+    const enteredAmountNumber = Number(enteredAmount);
+
+    if (
+      enteredAmount?.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         inputLabel='Amount'
         inputProps={{
           id: 'amount',
@@ -16,7 +43,8 @@ const MealItemForm = () => {
           defaultValue: '1',
         }}
       />
-      <button type='button'>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
+      <button type='submit'>+ Add</button>
     </form>
   );
 };
--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classes from './Input.module.css';
 
 interface Props {
   inputLabel: string;
   inputProps: Record<string, string>;
+  ref: React.RefObject<HTMLInputElement>;
 }
 
-const Input = ({ inputLabel, inputProps }: Props) => {
-  return (
-    <div className={classes.input}>
-      <label htmlFor={inputProps.id}>{inputLabel}</label>
-      <input {...inputProps} />
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ inputLabel, inputProps }, ref) => {
+    return (
+      <div className={classes.input}>
+        <label htmlFor={inputProps.id}>{inputLabel}</label>
+        <input ref={ref} {...inputProps} />
+      </div>
+    );
+  },
+);
 
 export default Input;
